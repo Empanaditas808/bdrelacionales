ae768a20908310d937dc9d59c6fa83cdddfd03d6
Empanaditas808 <angel.loaiza23@cetis107.edu.mx>
2025-06-20 18:34:08 -0700

    api_frostysino 20/06/2025

---
+++ Parcial 3/api_frostysino/getCasino.php
@@ -0,0 +1,17 @@
+<?php
+    
+    $host = 'localhost';
+    $user = 'root';
+    $pass = '';
+    $dbname = 'frostysino';
+
+    
+    //crear conexion
+    $conn = new mysqli($host, $user, $pass, $dbname);
+
+    //verificarconexion
+    if($conn->connect_error){
+        http_response_code(500);
+        echo json_encode(['error' => 'Error de conexion: ' . $conn->connect_error]);
+    }
+?>
